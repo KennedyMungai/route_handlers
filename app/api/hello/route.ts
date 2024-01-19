e3e19baae7ac51d5e3cc8189e0ef7c85387378d9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-19 11:03:22 +0300

    Made some updates to the GET function

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export async function GET() {
-    return new Response('Hello, NextJS')
+    return NextResponse.json({"message": "Hello, NextJS!!!"})
 }
