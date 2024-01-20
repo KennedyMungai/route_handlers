401670f547c22597e6f11a620edb1c0dc58faa99
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-20 08:47:02 +0300

    Added the endpoint code to get TODOS from the server

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,16 +1,13 @@
+import { NextResponse } from "next/server"
+
 const DATA_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos'
 
 
 export async function GET()
 {
-    const res = await fetch(DATA_SOURCE_URL, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        },
-    })
+    const res = await fetch(DATA_SOURCE_URL)
+    
+    const todos: Todo[] = await res.json()
 
-    const result = res.json()
-
-    console.log(result)
+    return NextResponse.json(todos)
 }
