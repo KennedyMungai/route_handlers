cd4c078b67c0f05801c0ccde32fff1c9b5ff739c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-21 10:50:54 +0300

    Added some more config options

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -3,18 +3,21 @@ import { limiter } from "../config/limiter"
 import { headers } from "next/headers"
 
 
-export async function GET() {
+export async function GET(request: Request) {
     const remaining = await limiter.removeTokens(1)
     console.log("Remaining", remaining)
+
+    const origin = request.headers.get('origin')
 
     if (remaining < 0) {
         return new NextResponse(
             "Too many requests",
             {
                 status: 429,
-                statusText: 'Too many requests', 
+                statusText: 'Too many requests',
                 headers: {
-                    'Access-Control-Allow-Origin': '*'
+                    'Access-Control-Allow-Origin': origin || '*',
+                    'Content-Type': 'text/plain'
                 }
             }
         )
