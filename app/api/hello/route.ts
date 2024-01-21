26649c4d2aeaecbc715e448a4ea9741e5811ef74
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-21 10:45:36 +0300

    Added an if statement for the remaining tokens that have run out

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -5,5 +5,10 @@ import { limiter } from "../config/limiter"
 export async function GET() {
     const remaining = await limiter.removeTokens(1)
     console.log("Remaining", remaining)
+
+    if (remaining < 0) {
+        return new NextResponse("Too many requests", { status: 429 })
+    }
+
     return NextResponse.json({ "message": "Hello, NextJS!!!" })
 }
