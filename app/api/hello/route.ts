159664014cb9fceed558baeb91fadc19406d2bd0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-21 10:48:41 +0300

    Added some config to the NextResponse instance for the remaining limiter tokens

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server"
 import { limiter } from "../config/limiter"
+import { headers } from "next/headers"
 
 
 export async function GET() {
@@ -7,7 +8,16 @@ export async function GET() {
     console.log("Remaining", remaining)
 
     if (remaining < 0) {
-        return new NextResponse("Too many requests", { status: 429 })
+        return new NextResponse(
+            "Too many requests",
+            {
+                status: 429,
+                statusText: 'Too many requests', 
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+        )
     }
 
     return NextResponse.json({ "message": "Hello, NextJS!!!" })
